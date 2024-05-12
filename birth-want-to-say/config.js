afc66d288bbe8add9789757437cf9ea280b2358c
Pi hongyi <165048419+phezecs@users.noreply.github.com>
2024-05-12 12:19:23 +0800

    Update config.js

--- birth-want-to-say/config.js
+++ birth-want-to-say/config.js
@@ -6,18 +6,7 @@ var config = {
         "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
         "心爱的姑娘",  // 同上...
         "今天是你的生日",
-        "这是我们在一起的",
-        "第三个生日！",
-        "这两年来，我见过你",
-        "奇怪的样子",
-        "可爱的样子",
-        "生气的样子",
-        "都是我喜欢的样子",
-        "对了，礼物收全了吗？",
-        "今年要吃好好的",
-        "喂饱饱的",
-        "然后才有力气干活",
-        "和想我！",
+
         "生日快乐~~",
     ],
     /**
@@ -33,18 +22,7 @@ var config = {
         "送给我": "./imgs/songxinxin.jpeg",
         "心爱的姑娘": "./imgs/nothing.jpg",
         "今天是你的生日": "./imgs/shengri.jpeg",
-        "这是我们在一起的": "./imgs/zaiyiqi.png",
-        "第三个生日！": "./imgs/nothing.jpg",
-        "这两年来，我见过你": "./imgs/yangzi.jpeg",
-        "奇怪的样子": "./imgs/nothing.jpg",
-        "可爱的样子": "./imgs/nothing.jpg",
-        "生气的样子": "./imgs/nothing.jpg",
-        "都是我喜欢的样子": "./imgs/nothing.jpg",
-        "对了，礼物收全了吗？": "./imgs/chuo.gif",
-        "今年要吃好好的": "./imgs/chihaodian.jpeg",
-        "喂饱饱的": "./imgs/weibao.jpeg",
-        "然后才有力气干活": "./imgs/nothing.jpg",
-        "和想我！": "./imgs/nothing.jpg",
+
         "生日快乐~~": "./imgs/nothing.jpg"
     },
     
